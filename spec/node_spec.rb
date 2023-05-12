1ab4391cd9094ede968bb7345741a36cb0a7bf21
Dylan Riesenman <djriesenman@gmail.com>
2023-05-12 13:28:04 -0600

    Add indentations for node_spec.rb

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,16 +2,22 @@ require "./lib/node"
 require 'rspec'
 
 describe Node do
-    describe "data" do
-        it 'returns requested data' do
-    node = Node.new("plop")
-    expect(node.data).to eq('plop')
-        end
-    end
-describe 'next node' do
-    it 'returns nil' do 
-        node = Node.new('plop')
-        expect(node.next_node).to be_nil
-end
-end
+	describe 'node exists' do
+		it 'exists' do
+			node = Node.new("plop")
+			expect(node).to be_a(Node)
+		end
+	end
+	describe "data" do
+		it 'returns requested data' do
+			node = Node.new('plop')
+			expect(node.data).to eq('plop')
+		end
+	end
+	describe 'next node' do
+		it 'returns nil' do 
+			node = Node.new('plop')
+			expect(node.next_node).to be_nil
+		end
+	end
 end
